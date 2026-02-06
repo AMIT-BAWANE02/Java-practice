e90f9155e3290f2737c40376700715caaa0f600e
AMIT-BAWANE02 <jaygoindabawane1234@gmail.com>
2026-02-06 18:04:42 +0530

    Remove compiled class files

--- OOPs/Oop/OopsMain/Cls/Animal.java
+++
@@ -1,9 +0,0 @@
-package Oop.OopsMain.Cls;
-
-public class Animal {
-    public void eat() {
-        System.out.println("Animal is eating");
-    }
-}
-
-
--- OOPs/Oop/OopsMain/Cls/Mouse.java
+++
@@ -1,9 +0,0 @@
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-package Oop.OopsMain.Cls;
-
-public class Mouse extends Animal {
-
-   public void squeak() {
-      System.out.println("Mouse says Squeak");
-   }
-}
